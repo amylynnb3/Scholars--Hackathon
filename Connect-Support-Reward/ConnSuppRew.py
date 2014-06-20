5cba17c1ab0a9e35e9ef1bd7b03a1d76dbe75181
Julien Gascon-Samson <zmaster17@gmail.com>
2014-06-20 14:07:05 -0700

    Added jinja2 template sample (hello world) :-)

--- Connect-Support-Reward/ConnSuppRew.py
+++ Connect-Support-Reward/ConnSuppRew.py
@@ -1,9 +1,21 @@
+import os
+import jinja2
 import webapp2
 
 class MainPage(webapp2.RequestHandler):
     def get(self):
-        self.response.headers['Content-Type'] = 'text/plain'
-        self.response.write('Hello, World!')
+        # Setup template values
+        template_values = {
+            'name': 'Julien',
+        }
+
+        template = JINJA_ENVIRONMENT.get_template('index.html')
+        self.response.write(template.render(template_values))
+
+JINJA_ENVIRONMENT = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+    extensions=['jinja2.ext.autoescape'],
+    autoescape=True)
 
 application = webapp2.WSGIApplication([
     ('/', MainPage),
