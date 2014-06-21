b3a140bcbbbb3da471c249a84a1c2ec4bbf03d03
--replace-all <nfarve@mit.edu>
2014-06-20 20:47:34 -0400

    adding users and signup for site

--- Connect-Support-Reward/ConnSuppRew.py
+++ Connect-Support-Reward/ConnSuppRew.py
@@ -1,18 +1,29 @@
 from google.appengine.api import users
-from google.appengine.ext import ndb
+from google.appengine.ext import db
+
 
 import os
 import jinja2
-
+import datetime
 import webapp2
+import cgi
 
 class MainPage(webapp2.RequestHandler):
     def get(self):
 
         user = users.get_current_user()
+        test = "Not a member"
+
         if user:
-            greeting = ('Welcome, %s! (<a href="%s">sign out</a>)' %
+
+            test = userAMember(user.user_id())
+            if test:
+                greeting = ('Welcome, %s! (<a href="%s">sign out</a>)' %
                         (user.nickname(), users.create_logout_url('/')))
+            else:
+                greeting="Null"
+                self.redirect("/join")
+                #add_user(user.nickname(), "Bob", "Test University", "Helping Others")
         else:
             greeting = ('<a href="%s">Sign in or register</a>.' %
                         users.create_login_url('/'))
@@ -20,23 +31,54 @@ class MainPage(webapp2.RequestHandler):
         # Setup template values
         template_values = {
             'greeting': greeting,
-            'name': 'Julien',
+            'name': test,
         }
 
         template = JINJA_ENVIRONMENT.get_template('index.html')
         self.response.write(template.render(template_values))
 
-class Member(ndb.Model):
-    userID= ndb.StringProperty(indexed=False)
-    fName=ndb.StringProperty(indexed=False)
-    homeSchool=ndb.StringProperty(indexed=True)
-    categories = ndb.StringProperty(indexed=True)
-    joinDate = ndb.DateTimeProperty(auto_now_add=True)
+class Join(webapp2.RequestHandler):
+    def get(self):
+        user= users.get_current_user()
+        greeting = ('Welcome, %s! (<a href="%s">sign out</a>)' %
+                        (user.user_id(), users.create_logout_url('/')))
+        template_values={
+            'greeting': greeting,
+        }
 
-def add_user(id, name, school, interest, date):
-    newUser = Member(userID = id, fname=name, homeSchool=school, categories=interest)
+        template = JINJA_ENVIRONMENT.get_template('completeProfile.html')
+        self.response.write(template.render(template_values))
+
+class Member(db.Model):
+    userID= db.StringProperty(indexed=True)
+    fName=db.StringProperty(indexed=False)
+    homeSchool=db.StringProperty(indexed=True)
+    categories = db.StringProperty(indexed=True)
+    joinDate = db.DateProperty(auto_now_add=True)
+
+def userAMember(user_id):
+    holder = Member.all()
+    holder.filter("userID = ", user_id)
+    if not holder.get():
+        return False
+    else: 
+        return True
+
+def add_user(userid, name, school, interest):
+    newUser = Member(userID = userid, fName=name, homeSchool=school, categories=interest)
     newUser.joinDate = datetime.datetime.now().date()
     newUser.put()
+
+class Signup(webapp2.RequestHandler):
+    def post(self):
+        user = users.get_current_user()
+        name = self.request.get('fname')
+        school = self.request.get('location')
+        interest = self.request.get_all('interest')
+        interest = ', '.join(interest)
+        add_user(user.user_id(), name, school, interest)
+        self.redirect("/")
+
 
 JINJA_ENVIRONMENT = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
@@ -45,5 +87,5 @@ JINJA_ENVIRONMENT = jinja2.Environment(
 
 
 application = webapp2.WSGIApplication([
-    ('/', MainPage),
+    ('/', MainPage),('/join',Join), ('/signup', Signup),
 ], debug=True)
